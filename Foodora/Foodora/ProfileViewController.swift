1556438ff13cd03fed3b40f3dec27569e62bb86e
Brandon Danis <bdani093@uottawa.ca>
2018-03-04 17:14:01 -0500

    Added placeholder labels

--- Foodora/Foodora/ProfileViewController.swift
+++ Foodora/Foodora/ProfileViewController.swift
@@ -11,12 +11,39 @@ import UIKit
 
 class ProfileViewController : UIViewController {
     
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.attributedText = NSAttributedString(string: "Profile View", attributes: [
+            NSAttributedStringKey.font: UIFont(name: "Avenir Next", size: 34)!,
+            NSAttributedStringKey.foregroundColor: UIColor.black
+        ])
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        ApplyConstraints()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    private func ApplyConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // Title label constraints
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
 }
