588144fe5dd839327574717710da2f8df69a60aa
Brandon Danis <bdani093@uottawa.ca>
2018-03-25 14:33:42 -0400

    Started login view

--- Foodora/Foodora/Style.swift
+++ Foodora/Foodora/Style.swift
@@ -11,4 +11,8 @@ import UIKit
 
 struct Style {
     static let main_color = UIColor(red:0.91, green:0.25, blue:0.26, alpha:1.00)
+    static let SOFT_BLUE = UIColor(red:0.15, green:0.57, blue:0.85, alpha:1.0)
+    static let SOFT_PURPLE = UIColor(red:0.49, green:0.31, blue:0.82, alpha:1.00)
+    static let DUSK_BLUE = UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.0)
+    static let DUSK_PURPLE = UIColor(red:0.99, green:0.45, blue:0.42, alpha:1.0)
 }
