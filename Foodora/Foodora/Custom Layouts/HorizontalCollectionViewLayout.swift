a7d493ddc3ecaa5571a793b76dc13dc7793aa1fe
Brandon Danis <bdani093@uottawa.ca>
2018-09-24 23:34:09 -0400

    Init HorizontalCollectionViewLayout

---
+++ Foodora/Foodora/Custom Layouts/HorizontalCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  HorizontalCollectionViewLayout.swift
+//  Foodora
+//
+//  Created by Brandon Danis on 2018-09-24.
+//  Copyright © 2018 FoodoraInc. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalCollectionViewLayout: UICollectionViewFlowLayout {
+
+}
