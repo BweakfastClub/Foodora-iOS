a7687b8e9dd7b1db76d3a07dfc7e36c971da2506
Brandon Danis <bdani093@uottawa.ca>
2018-11-16 13:36:56 -0500

    Added meal plan functions to user model

--- Foodora/Foodora/Models/User.swift
+++ Foodora/Foodora/Models/User.swift
@@ -13,11 +13,44 @@ class User : Codable {
     var name: String
     var email: String
     var likedRecipes: [Meal]?
+    var mealPlanDict: [String:[Meal]]?
     
     enum CodingKeys: String, CodingKey {
         case name = "name"
         case email = "email"
         case likedRecipes = "likedRecipes"
+        case mealPlanDict = "mealPlan"
+    }
+    
+}
+
+extension User {
+    
+    public func AddTopMealPlan(mealToAdd: Meal, typeOfMeal: String, _ callback: @escaping (_ success: Bool) -> Void) {
+        NetworkManager.shared.AddToMealPlan(mealToAdd.mealId, typeOfMeal, false, callback: callback)
+    }
+    
+    public func RemoveFromMealPlan(mealToAdd: Meal, typeOfMeal: String, _ callback: @escaping (_ success: Bool) -> Void) {
+        NetworkManager.shared.AddToMealPlan(mealToAdd.mealId, typeOfMeal, true, callback: callback)
+    }
+    
+    public func GetMealPlanBreakfast() -> [Meal] {
+        return GetMealsFromDict(key: "breakfast")
+    }
+    
+    public func GetMealPlanLunch() -> [Meal] {
+        return GetMealsFromDict(key: "lunch")
+    }
+    
+    public func GetMealPlanDinner() -> [Meal] {
+        return GetMealsFromDict(key: "dinner")
+    }
+    
+    private func GetMealsFromDict(key: String) -> [Meal] {
+        guard let dict = mealPlanDict, let meals = dict[key] else {
+            return []
+        }
+        return meals
     }
     
 }
